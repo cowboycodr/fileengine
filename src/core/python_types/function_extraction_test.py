b775442daab896ad773c5ffe7b590860e89b582d
Kian McKenna <kian.p.m.007@gmail.com>
2021-05-24 22:24:39 -0500

    remove uneccessary files and code

--- src/core/python_types/function_extraction_test.py
+++
@@ -1,8 +0,0 @@
-def do_nothing(self, nothing: None = None, this: bool = True) -> None:
-    ok_world = 'Kian McKenna'
-
-def last_name(name: str = 'Kian') -> str:
-    if name == 'Kian':
-        return 'McKenna'
-
-ok = True
