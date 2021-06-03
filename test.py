027ce201a52e8b26ede4c5b4612043806f7ff0f9
Kian <kian.p.m.007@gmail.com>
2021-06-03 00:22:17 -0500

    remove uneeded files

--- test.py
+++
@@ -1,9 +0,0 @@
-from src.file_engine import FE
-
-if __name__ == '__main__':
-    myFile = FE.create(r'D:\code\dev\python-dev\file-engine\kian_mckenna.txt')
-    file = FE.grab(myFile.get_filepath())
-
-    file.insert(10, r'\n')
-    file.insert(1, 'dog')
-    file.overwrite()
